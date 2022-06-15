0b6401084a2af13a453224a5320f979138289885
mat557 <matasnim11@gmail.com>
2022-06-15 13:41:32 +0600

    one api created

--- index.js
+++ index.js
@@ -2,12 +2,45 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pyrpo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+  try{
+    await client.connect();
+    const userCollection = client.db('spectrum-collection').collection('users');
+    
+
+
+    app.put('/users/:email',async(req,res)=>{
+      const email = req.params.email;
+      const user = req.body;
+      console.log(user,email)
+      const filter = {email:email}
+      const options = { upsert : true}
+      const updateDoc = {
+        $set:user,
+      }
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    })
+  }
+  finally{}
+}
+
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
   res.send('This is from backend of spectrum');
